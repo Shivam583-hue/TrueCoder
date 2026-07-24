1ddcf79b84bb54546f15e96540e3395468105801
Shivam583-hue <shivamshivamshivam456@gmail.com>
2026-07-24 08:27:27 +0600

    Completed streaming and non streaming response scafolding.

--- src/truecoder/__main__.py
+++ src/truecoder/__main__.py
@@ -6,7 +6,8 @@ from truecoder.client.llm_client import LLMClient
 async def main():
     client = LLMClient()
     messages = [{"role": "user", "content": "What's up"}]
-    await client.chat_completion(messages, False)
+    async for event in client.chat_completion(messages, True):
+        print(event)
     print("done")
 
 
--- src/truecoder/client/llm_client.py
+++ src/truecoder/client/llm_client.py
@@ -1,8 +1,11 @@
 import os
-from typing import Any
+from re import finditer
+from typing import Any, AsyncGenerator
 
 from dotenv import load_dotenv
 from openai import AsyncOpenAI
+
+from truecoder.client.response import EventType, StreamEvent, TextDelta, TokenUsage
 
 load_dotenv()
 
@@ -29,7 +32,7 @@ class LLMClient:
         self,
         messages: list[dict[str, Any]],
         stream: bool = True,
-    ):
+    ) -> AsyncGenerator[StreamEvent, None]:
         client = self.get_client()
         keywargs = {
             "model": os.getenv("MODEL"),
@@ -37,13 +40,71 @@ class LLMClient:
             "stream": stream,
         }
         if stream:
-            await self._stream_response()
+            async for event in self._stream_response(client, keywargs):
+                yield event
         else:
-            await self._non_stream_reponse(client, keywargs)
+            event = await self._non_stream_response(client, keywargs)
+            yield event
+        return
 
-    async def _stream_response(self):
-        pass
-
-    async def _non_stream_reponse(self, client: AsyncOpenAI, keywargs: dict[str, Any]):
+    async def _stream_response(
+        self, client: AsyncOpenAI, keywargs: dict[str, Any]
+    ) -> AsyncGenerator[StreamEvent, None]:
         response = await client.chat.completions.create(**keywargs)
-        print(response)
+
+        usage: TokenUsage | None = None
+        finish_reason: str | None = None
+
+        async for chunk in response:
+            if hasattr(chunk, "usage") and chunk.usage:
+                usage = TokenUsage(
+                    prompt_tokens=chunk.usage.prompt_tokens,
+                    completion_tokens=chunk.usage.completion_tokens,
+                    total_tokens=chunk.usage.total_tokens,
+                    cached_tokens=chunk.usage.prompt_tokens_details.cached_tokens,
+                )
+            if not chunk.choices:
+                continue
+            choice = chunk.choices[0]
+            delta = choice.delta
+
+            if choice.finish_reason:
+                finish_reason = choice.finish_reason
+
+            if delta.content:
+                yield StreamEvent(
+                    type=EventType.TEXT_DELTA,
+                    text_delta=TextDelta(delta.content),
+                )
+
+        yield StreamEvent(
+            type=EventType.MESSAGE_COMPLETE,
+            finish_reason=finish_reason,
+            usage=usage,
+        )
+
+    async def _non_stream_response(
+        self, client: AsyncOpenAI, keywargs: dict[str, Any]
+    ) -> StreamEvent:
+        response = await client.chat.completions.create(**keywargs)
+        choice = response.choices[0]
+        message = choice.message
+
+        text_delta = None
+        if message.content:
+            text_delta = TextDelta(content=message.content)
+
+        usage = None
+        if response.usage:
+            usage = TokenUsage(
+                prompt_tokens=response.usage.prompt_tokens,
+                completion_tokens=response.usage.completion_tokens,
+                total_tokens=response.usage.total_tokens,
+                cached_tokens=response.usage.prompt_tokens_details.cached_tokens,
+            )
+        return StreamEvent(
+            type=EventType.MESSAGE_COMPLETE,
+            text_delta=text_delta,
+            finish_reason=choice.finish_reason,
+            usage=usage,
+        )
