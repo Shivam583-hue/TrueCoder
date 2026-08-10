a74ff8511c4b0faa050c404e4acdf0cffd595048
Shivam583-hue <shivamshivamshivam456@gmail.com>
2026-08-10 22:30:04 +0600

    fix(tests): make POSIX tree readiness deterministic

--- tests/helpers/execution/spawn_tree.py
+++ tests/helpers/execution/spawn_tree.py
@@ -5,9 +5,10 @@ import os
 import signal
 import time
 
+signal.signal(signal.SIGTERM, signal.SIG_IGN)
+
 
 def wait_forever() -> None:
-    signal.signal(signal.SIGTERM, signal.SIG_IGN)
     while True:
         time.sleep(1)
 
